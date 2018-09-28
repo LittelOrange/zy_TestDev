6fe5ca13e98f1d34eab6742ad6b9f4e90ec9267e
LittelOrange <1024351971@qq.com>
2018-09-28 21:37:20 +0800

    登录提交

---
+++ test_platform/user_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import auth
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+#处理登录请求
+def login_action(request):
+    if request.method == "POST":
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+
+        if username == "" or password == "":
+            return render(request, "index.html",
+                          {"error": "用户名或者密码为空"}
+                          )
+        else:
+            user = auth.authenticate(username = username,password = password)
+            # print(user)
+            # print(type(user))
+            if user is not None:
+                auth.login(request, user)
+                return render(request, "project_manage.html")
+            else:
+                return render(request, "index.html",
+                              {"error": "用户名或密码错误"}
+                              )
+
+
+
+
+
+
