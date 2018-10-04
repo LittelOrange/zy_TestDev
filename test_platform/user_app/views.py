b09b1dd96f3338bc11d54702319555207a99b069
LittelOrange <1024351971@qq.com>
2018-10-04 16:03:12 +0800

    添加主要菜单；增加退出功能

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -26,18 +27,27 @@ def login_action(request):
                 response = HttpResponseRedirect("/project_manage/")
                 # response.set_cookie('user', username, 3600) #添加浏览器cookie
                 return response
-
-                # return render(request, "project_manage.html",{"user": user })
             else:
                 return render(request, "index.html",
                               {"error": "用户名或密码错误"}
                               )
 
-
+@login_required  #判断用户是否登录
 def project_manage(request):
     username = request.session.get('user','') #读取浏览器session
     # username = request.COOKIES.get('user','')  #读取浏览器cookie
     return render(request, "project_manage.html",{"user":username})
+
+
+def logout(request):
+    """
+        退出登录
+    :param request:
+    :return:
+    """
+    auth.logout(request)  #清除用户的登录状态
+    response = HttpResponseRedirect("/")
+    return response
 
 
 
