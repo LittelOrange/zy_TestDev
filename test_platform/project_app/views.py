2b8347bab46fa01cc1c3a89faf3576d6bb1feaf4
LittelOrange <1024351971@qq.com>
2018-10-23 06:43:08 +0800

    添加forms.py文件

--- test_platform/project_app/views.py
+++ test_platform/project_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from project_app.models import Project,Module
+from .forms import ProjectForm
+
 
 # Create your views here.
 @login_required  #判断用户是否登录
@@ -18,13 +21,35 @@ def project_manage(request):
     })
 
 
+# @login_required
+# def add_project(request):
+#     '''
+#     添加项目
+#     '''
+#     return render(request, "project_manage.html",
+#                   {"type":"add"},
+#                   )
+
+
 @login_required
 def add_project(request):
-    '''
-    添加项目
-    '''
-    return render(request, "project_manage.html",
-                  {"type":"add"},
-                  )
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ProjectForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
 
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ProjectForm()
 
+    return render(request,
+                  "project_manage.html",{
+                      'form':form,
+                      "type":"add",
+                  })
