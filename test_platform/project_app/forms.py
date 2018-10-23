2aa11d7beb27e7def033d6796f155e9117bd86cc
LittelOrange <1024351971@qq.com>
2018-10-23 17:50:22 +0800

    添加项目，新增编辑、删除功能

--- test_platform/project_app/forms.py
+++ test_platform/project_app/forms.py
@@ -1,6 +1,21 @@
 from django import forms
+from .models import Project, Module
 
-class ProjectForm(forms.Form):
+# class ProjectForm(forms.Form):
+#
+#     name = forms.CharField(label = '名称',max_length = 100)
+#     describe = forms.CharField(label="描述", widget=forms.Textarea)
+#     status = forms.BooleanField(label="状态",rquest = False)
 
-    name = forms.CharField(label = '名称',max_length = 100)
-    describe = forms.CharField(label="描述", widget=forms.Textarea)
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name', 'describe', 'status']
+        # exclude = ['create_time']
+
+
+class ModuleForm(forms.ModelForm):
+    class Meta:
+        model = Module
+        exclude = ['create_time']
--- test_platform/project_app/views.py
+++ test_platform/project_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from project_app.models import Project,Module
+from project_app.models import Project
 from .forms import ProjectForm
 
 
@@ -39,10 +39,11 @@ def add_project(request):
         form = ProjectForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            name = form.cleaned_data['name']
+            describe = form.cleaned_data['describe']
+            status = form.cleaned_data['status']
+            Project.objects.create(name=name, describe=describe, status=status)
+            return HttpResponseRedirect('/manage/project_manage/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -53,3 +54,38 @@ def add_project(request):
                       'form':form,
                       "type":"add",
                   })
+
+
+@login_required
+def edit_project(request, pid):
+    """
+    编辑项目
+    """
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            model = Project.objects.get(id=pid)
+            model.name = form.cleaned_data['name']
+            model.describe = form.cleaned_data['describe']
+            model.status = form.cleaned_data['status']
+            model.save()
+            return HttpResponseRedirect('/manage/project_manage/')
+    else:
+        if pid:
+            form = ProjectForm(
+                instance=Project.objects.get(id=pid)
+            )
+
+    return render(request, 'project_manage.html', {
+        'form': form,
+        "type": "edit",
+    })
+
+
+@login_required
+def delete_project(request, pid):
+    """
+    删除项目
+    """
+    Project.objects.get(id=pid).delete()
+    return HttpResponseRedirect("/manage/project_manage/")
