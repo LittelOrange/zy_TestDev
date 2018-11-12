e2a592b8097e4d96fe38484d2e99cbe2591b5cfb
LittelOrange <1024351971@qq.com>
2018-11-12 23:56:09 +0800

    在api_debug.html中添加保存按钮

--- test_platform/interface_app/admin.py
+++ test_platform/interface_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from interface_app.models import TestCase
 # Register your models here.
+class TestCaseAdmin(admin.ModelAdmin):
+    list_display = [ "module", "name", "url","req_method","req_type","req_header","req_parameter","response_assert","create_time", "id"]
+
+admin.site.register(TestCase,TestCaseAdmin)
+
--- test_platform/interface_app/views.py
+++ test_platform/interface_app/views.py
@@ -3,6 +3,7 @@ import requests
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from interface_app.forms import TestCaseForm
 
 # Create your views here.
 def case_manage(request):
@@ -16,25 +17,25 @@ def case_manage(request):
 
 #创建接口
 def debug(request):
+    form = TestCaseForm()
     if request.method == "GET":
         return render(request,"api_debug.html",
-                      {"type": "debug"
+                      {"form":form,
+                       "type": "debug",
         })
     else:
         return HttpResponse("404")
 
 #调试接口
 def api_debug(request):
+
     if request.method == "POST":
         url = request.POST.get("req_url")
         method = request.POST.get("req_method")
         parameter = request.POST.get("req_parameter")
-        # print(parameter)
-        # print(type(parameter))
+        
         # payload = json.loads(parameter)
         payload = json.loads(parameter.replace("'", "\""))
-        # print(payload)
-        # print(type(payload))
 
         if method == "get":
             r = requests.get(url,params=payload)  #此处必须为requests，不能乱写，是导入的包
