46d7e25c78fd7a6a1b7bc6207118dcbf4d0b307a
LittelOrange <1024351971@qq.com>
2018-11-14 15:10:43 +0800

    增加分页及搜索功能

--- test_platform/interface_app/views.py
+++ test_platform/interface_app/views.py
@@ -4,16 +4,63 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from interface_app.forms import TestCaseForm
+from interface_app.models import TestCase
 from project_app.models import Project, Module
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # Create your views here.
+#用例列表
 def case_manage(request):
+    testcases = TestCase.objects.all()
+    paginator = Paginator(testcases, 5) #每页显示5条数据
+
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果页数不是整型, 取第一页.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # 如果页数超出查询范围，取最后一页
+        contacts = paginator.page(paginator.num_pages)
+
     if request.method == "GET":
         return render(request,"case_manage.html",
-                      {"type": "list"
+                      {"type": "list",
+                       "testcases":contacts,
         })
     else:
         return HttpResponse("404")
+
+#按“用例名称”搜索用例
+def search_case_name(request):
+
+    if request.method == "GET":
+        case_name = request.GET.get('case_name', "")
+        cases = TestCase.objects.filter(name__contains=case_name)
+
+        paginator = Paginator(cases, 5)  #每页显示5条数据
+        page = request.GET.get('page')
+        try:
+            contacts = paginator.page(page)
+        except PageNotAnInteger:
+            # 如果页数不是整型, 取第一页.
+            contacts = paginator.page(1)
+        except EmptyPage:
+            # 如果页数超出查询范围，取最后一页
+            contacts = paginator.page(paginator.num_pages)
+
+        return render(request,"case_manage.html",
+                      {"type": "list",
+                       "testcases":contacts,
+                       "case_name": case_name,
+        })
+    else:
+        return HttpResponse("404")
+
+
+
 
 
 #创建/tiaoshi接口
@@ -36,7 +83,7 @@ def api_debug(request):
         parameter = request.POST.get("req_parameter")
         
         # payload = json.loads(parameter)
-        payload = json.loads(parameter.replace("'", "\""))
+        payload = json.loads(parameter.replace("'", "\""))   #loads()：将字符串转换为字典;replace()：将单引号替换为双引号
 
         if method == "get":
             r = requests.get(url,params=payload)  #此处必须为requests，不能乱写，是导入的包
@@ -71,10 +118,14 @@ def save_case(request):
 
         module_obj = Module.objects.get(name=module_name)
 
-        case = TestCase.objects.create(name=name, module=module_obj, url=url,
-                                       req_method=method, req_header=header,
+        case = TestCase.objects.create(name=name,
+                                       module=module_obj,
+                                       url=url,
+                                       req_method=method,
+                                       req_header=header,
                                        req_type=req_type,
-                                       req_parameter=parameter)
+                                       req_parameter=parameter
+                                       )
         if case is not None:
             return HttpResponse("保存成功！")
 
